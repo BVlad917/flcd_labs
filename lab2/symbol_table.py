30b10843b9df35a27e0fb9422f40e2a574b03e80
VladBogdan <bogdanvlad2001@yahoo.com>
2022-10-24 01:00:43 +0300

    finished lab

--- lab2/symbol_table.py
+++ lab2/symbol_table.py
@@ -1,28 +1,52 @@
 from collections import defaultdict
 
-DEFAULT_SIZE = 17
+DEFAULT_CAPACITY = 17
 
 
 class SymbolTable:
-    def __init__(self):
-        self.__hashtable = defaultdict(list)
-        self.__m = DEFAULT_SIZE
+    def __init__(self, table_size=DEFAULT_CAPACITY):
+        self.__hash_table = defaultdict(list)
+        self.__capacity = table_size
+        self.__size = 0
 
-    def add_elem(self, elem):
-        key, elem_index = self.find_element_position(elem)
+    def add_elem(self, symbol):
+        """
+        Add a new symbol to the symbol table. If the element already exists, throw ValueError
+        :param symbol: the new symbol to insert in the symbol table; number or string
+        """
+        key, elem_index = self.find_symbol_position(symbol)
         if elem_index != -1:
             raise ValueError("Element already in the symbol table")
         else:
-            self.__hashtable[key].append(elem)
+            self.__hash_table[key].append(symbol)
+            self.__size += 1
 
-    def find_element_position(self, elem):
-        key = self.h(elem)
-        if elem in self.__hashtable[key]:
-            return key, self.__hashtable[key].index(elem)
+    def get_size(self):
+        """
+        :return: the current size (number of symbols) of the symbol table
+        """
+        return self.__size
+
+    def find_symbol_position(self, symbol):
+        """
+        Find the position of the symbol in the symbol table
+        :param symbol: the symbol which should be searched; number or string
+        :return: a pair with the position in the hash table at first position, and position in the collision list
+        (if the symbol exists) or -1 (if the symbol doesn't exist) at the second position
+        """
+        key = self.__h(symbol)
+        if symbol in self.__hash_table[key]:
+            return key, self.__hash_table[key].index(symbol)
         else:
             return key, -1
 
-    def h(self, value):
-        str_value = str(value)
+    def __h(self, symbol):
+        """
+        Hash function. Adds the ASCII values of the characters in the given symbol, finds the ASCII sum, and
+        computes the modulo with the argument <m> of the symbol table
+        :param symbol: input to find the hash value for; number or string
+        :return: hash value for the given input; int
+        """
+        str_value = str(symbol)
         ascii_sum = sum([ord(c) for c in str_value])
-        return ascii_sum % self.__m
+        return ascii_sum % self.__capacity
--- lab2/tests.py
+++ lab2/tests.py
@@ -1,26 +1,42 @@
 from symbol_table import SymbolTable
 
 st = SymbolTable()
-st.add_elem("test")
-assert st.find_element_position("test") == (6, 0)
 
+# add an element in the symbol table and check that it was added on the correct position according to
+# the chosen hash function
+st.add_elem("test")
+assert st.find_symbol_position("test") == (6, 0)
+
+# add a few more elements and check their positions, none have collisions
 st.add_elem("a")
 st.add_elem("b")
 st.add_elem("c")
-assert st.find_element_position("a") == (12, 0)
-assert st.find_element_position("b") == (13, 0)
-assert st.find_element_position("c") == (14, 0)
+assert st.find_symbol_position("a") == (12, 0)
+assert st.find_symbol_position("b") == (13, 0)
+assert st.find_symbol_position("c") == (14, 0)
 
+# add some more elements, one of them will have a collision with a previously added element
 st.add_elem(-2)
 st.add_elem(0)
 st.add_elem(1)
 
-assert st.find_element_position(-2) == (10, 0)
-assert st.find_element_position(0) == (14, 1)    # "0" % m has the same ascii sum as "c", so we have a collision
-assert st.find_element_position(1) == (15, 0)
+assert st.find_symbol_position(-2) == (10, 0)
+assert st.find_symbol_position(0) == (14, 1)    # "0" % m has the same ascii sum as "c", so we have a collision
+assert st.find_symbol_position(1) == (15, 0)
 
+# add string elements as well
 st.add_elem("print")
 st.add_elem("nice_message")
 
-assert st.find_element_position("print") == (13, 1)    # same ascii sum as "b"
-assert st.find_element_position("nice_message") == (10, 1)   # same ascii sum as "-2"
+assert st.find_symbol_position("print") == (13, 1)    # same ascii sum as "b"
+assert st.find_symbol_position("nice_message") == (10, 1)   # same ascii sum as "-2"
+
+# search for elements which were not added in the symbol table and check that the output is right
+assert st.find_symbol_position("non-existing-string")[1] == -1
+
+# check that trying to add an existing element raises an exception
+try:
+    st.add_elem(0)
+    assert False
+except ValueError:
+    assert True
